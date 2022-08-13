ea6bcecb46e6e319316ba0c67c51ad289927f763
Kusal pal <110298805+Kusalo12001@users.noreply.github.com>
2022-08-13 21:12:02 +0530

    Add files via upload

---
+++ 14.find power.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int x,n;
+    cout<<"enter x: ";
+    cin>>x;
+    cout<<"enter n: ";
+    cin>>n;
+    //double power = pow(x,n);
+    cout<<"power: "<<pow(x,n);
+}
