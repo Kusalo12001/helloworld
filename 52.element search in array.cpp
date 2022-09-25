25c21fd207841bf299cdff3e57b405a229a957d4
Kusal pal <110298805+Kusalo12001@users.noreply.github.com>
2022-09-25 10:49:23 +0530

    Add files via upload

---
+++ 52.element search in array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[50];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==15){
+            //cout<<&arr[i];
+            break;
+        }
+        if (i < n)
+    {
+        cout <<
+    }
+    else {
+        cout << -1;
+    }
+
+    }
+}
