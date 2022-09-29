ff83df270fd547f3fcab139abfcb48293a3427b4
Kusal pal <110298805+Kusalo12001@users.noreply.github.com>
2022-09-29 19:15:46 +0530

    Add files via upload

---
+++ 57.element are present or not in array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+bool Search(int arr[],int n,int key)
+{
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int key;
+    cin>>key;
+    int arr[10]={54,65,74,75,90,4,23,54,6,3};
+    /*for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }*/
+    bool found=Search(arr,10,key);
+    if(found){
+        cout<<"key is present"<<endl;
+    }
+    else{
+        cout<<"key is absent";
+    }
+}
