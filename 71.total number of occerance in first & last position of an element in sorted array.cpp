1d1a10de1ab1629aa23ff5036b9afb8cd8a2e7f4
Kusal pal <110298805+Kusalo12001@users.noreply.github.com>
2022-10-13 19:48:58 +0530

    Add files via upload

---
+++ 71.total number of occerance in first & last position of an element in sorted array.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int LastPosition(int arr[],int n,int key)
+{
+    int s=0;
+    int e=n-1;
+    int ans=-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans=mid;
+            s=mid+1;
+        }
+        else if(arr[mid]<key){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+int FirstPosition(int arr[],int n,int key)
+{
+    int s=0;
+    int e=n-1;
+    int ans=-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans=mid;
+            e=mid-1;
+        }
+        else if(arr[mid]<key){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+int main()
+{
+    int arr[10]={1,3,4,5,5,5,5,6,7,9};
+    //int FirstPosition(arr,10,5);
+    int index1=FirstPosition(arr,10,5);
+    int index2=LastPosition(arr,10,5);
+    int numberofocc=index2-index1+1;
+    //cout<<index1<<endl<<index2<<endl;
+    cout<<numberofocc;
+
+}
+
+
